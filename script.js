763efa1c704de8db5e0fcec567eb29b087696628
Gin <181115100+bogfrog37@users.noreply.github.com>
2024-12-07 10:53:01 -0500

    Update script.js
    improvements

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function toggleSidebar() {
+$(document).ready(function() {
+
+  // Function to toggle the sidebar visibility
+  function toggleSidebar() {
     // If sidebar is open, close it; if it's closed, open it
     $('#sideNav').toggleClass('open');
     $('.content').toggleClass('shifted'); // Move content when sidebar is open
@@ -7,58 +10,51 @@ function toggleSidebar() {
   // Attach toggleSidebar function to hamburger button click event
   $('.hamburger-btn').click(function() {
     toggleSidebar();
+  });
+
+  // Image switching functionality
+  const images = ['sheep.jpeg', 'Cows.jpeg']; // Add your image paths here
+  let currentImageIndex = 0;
+
+  function switchImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    $('#image').attr('src', images[currentImageIndex]); // Using jQuery to update the src attribute
   }
-}
-                      
 
+  // Set the interval to switch images every 3 seconds (3000 milliseconds)
+  setInterval(switchImage, 3000);
 
-    $(document).ready(function() {
-      const images = ['sheep.jpeg', 'Cows.jpeg']; // Add your image paths here
-      let currentImageIndex = 0;
-    
-      function switchImage() {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        $('#image').attr('src', images[currentImageIndex]); // Using jQuery to update the src attribute
-      }
-    
-      // Set the interval to switch images every 3 seconds (3000 milliseconds)
-      setInterval(switchImage, 3000);
-    });
-
-   // Change Link Color on Hover
-   $('.link').hover(function () {
-     $(this).css('color', '#005f73');
-   }, function () {
-     $(this).css('color', '');
-   });
-
-   $(document).ready(function () {
-    // When hovering over job list items
-    $('.job-list li').hover(function () {
-      // Get the description from the 'data-description' attribute
-      const description = $(this).data('description');
-  
-      // Display the description in the '.job-description' div
-      $('.job-description').text(description).fadeIn();
-    }, function () {
-      // Hide the description when hover ends
-      $('.job-description').fadeOut();
-    });
+  // Change Link Color on Hover
+  $('.link').hover(function () {
+    $(this).css('color', '#005f73');
+  }, function () {
+    $(this).css('color', ''); // Reset the color when hover ends
   });
-  
-  $(document).ready(function() {
-    // Add a blue vertical line to the left side of the body content
-    $('body').prepend('<div id="left-line"></div>');  // Add left line
 
-    // Style the left line using jQuery
-    $('#left-line').css({
-      'position': 'fixed',   // Fixed position to keep it on the left side while scrolling
-      'top': '0',            // Start from the top of the page
-      'bottom': '0',         // Extend to the bottom of the page
-      'width': '5px',        // Thickness of the line
-      'background-color': '#005f73', // Line color
-      'z-index': '9999',     // Ensure the line is on top of content
-      'left': '0'            // Position the line on the left side
-    });
+  // Job list hover functionality to show descriptions
+  $('.job-list li').hover(function () {
+    // Get the description from the 'data-description' attribute
+    const description = $(this).data('description');
+
+    // Display the description in the '.job-description' div
+    $('.job-description').text(description).fadeIn();
+  }, function () {
+    // Hide the description when hover ends
+    $('.job-description').fadeOut();
   });
-  
+
+  // Add a blue vertical line to the left side of the body content
+  $('body').prepend('<div id="left-line"></div>');  // Add left line
+
+  // Style the left line using jQuery
+  $('#left-line').css({
+    'position': 'fixed',   // Fixed position to keep it on the left side while scrolling
+    'top': '0',            // Start from the top of the page
+    'bottom': '0',         // Extend to the bottom of the page
+    'width': '5px',        // Thickness of the line
+    'background-color': '#005f73', // Line color
+    'z-index': '9999',     // Ensure the line is on top of content
+    'left': '0'            // Position the line on the left side
+  });
+
+});
